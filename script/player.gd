f56b97e5d049998a6401bf6510b6740f09557eb7
Lucas Vasques <lucasedvasques@gmail.com>
2026-08-19 16:52:54 -0300

    implemented shot
    -implemented aim
    -implemented shot
    -implemented ray cast
    -implemented buttons

--- script/player.gd
+++ script/player.gd
@@ -1,5 +1,8 @@
 extends CharacterBody3D
 
+const IMPACT_MESH = preload("uid://dno72hdvohili")
+
+
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
@@ -7,6 +10,7 @@ var last_mouse_position: Vector2i
 var mouse_sens: float = 0.01
 
 @onready var fpp_camera: Camera3D = $FPPCamera
+
 
 
 func _ready() -> void:
@@ -18,6 +22,8 @@ func _unhandled_input(event: InputEvent) -> void:
 		rotate_y(-event.screen_relative.x * mouse_sens)
 		fpp_camera.rotation.x += -event.screen_relative.y * mouse_sens
 		fpp_camera.rotation.x = clampf(fpp_camera.rotation.x, -PI/2, PI/2) 
+	if event.is_action_pressed("attack"):
+		attack()
 	
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -25,13 +31,13 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -41,3 +47,19 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+	
+func attack() -> void:
+		var space_state := get_world_3d().direct_space_state
+		var cam:= get_viewport().get_camera_3d()
+		var mouse_position := get_viewport().get_mouse_position()
+		var ray_origin: Vector3 = cam.project_ray_origin(mouse_position)
+		var ray_direction: Vector3 = ray_origin + cam.project_ray_normal(mouse_position) *100
+		
+		var query := PhysicsRayQueryParameters3D.create(ray_origin, ray_direction, collision_mask)
+		var result:= space_state.intersect_ray(query)
+		print(result)
+		
+		if not result.is_empty():
+			var impact_mesh := IMPACT_MESH.instantiate()
+			add_sibling(impact_mesh)
+			impact_mesh.global_position = result["position"]
