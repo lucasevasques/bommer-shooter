c79123f112dd01f66f9588cb74bc05840aac7817
Lucas Vasques <lucasedvasques@gmail.com>
2026-09-02 16:46:46 -0300

    Enemy take_damage
    -Implemented enemy hp
    -Implemented enemy take_damage
    -Implemented enemy death animation
    -implemented hit particles on enemy

--- script/enemy.gd
+++ script/enemy.gd
@@ -3,8 +3,19 @@ extends CharacterBody3D
 
 signal player_requested
 
+@export_range(0, 100, 1, "or_greater") var hp: int:
+	set(value):
+		hp = value
+		if  hp <= 0:
+			die()
+
 var player: Node3D
+var is_dying: bool = false
+
 @onready var raycast: RayCast3D = $RayCast3D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var nav_agent: NavigationAgent3D = $NavigationAgent3D
+@onready var hit_particles: GPUParticles3D = $HitParticles
 
 
 
@@ -16,12 +27,29 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if player == null:
 		return
-	raycast.global_position = global_position + Vector3(0, 1.5,0)
-	var look_target: Vector3 = player.global_position + Vector3(0, 2, 0)
+	if is_dying == true:
+		return
+	raycast.global_position = global_position + Vector3(0, 1.5, 0)
+	var look_target: Vector3 = player.global_position + Vector3(0, 1, 0)
 	raycast.look_at(look_target, Vector3.UP, true)
 	
-	var rotation_target: Vector3 = raycast.global_rotation
-	rotation_target *= Vector3(0,1,0)
-	rotation_target = rotation_target.normalized()
-	
-	global_rotation.y = raycast.global_position.y
+	global_rotation.y = raycast.global_rotation.y
+	print(player, " ", raycast)
+	if raycast.is_colliding():
+		if "Player" in raycast.get_collider().name:
+			print("To te vendo")
+		
+
+func take_damage(amount: float) -> void:
+	if is_dying == true:
+		return
+	hit_particles.restart()
+	hp -= amount
+	if hp <=0:
+		die()
+		
+		
+func die() -> void:
+	if is_dying == false:
+		is_dying = true
+	animation_player.play("death")
